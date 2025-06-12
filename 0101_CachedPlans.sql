d49645375eb00ae878072b852c3e1df190075578
Rauchendraser <andreasr@ppedv.ed>
2025-06-12 10:52:31 +0200

    Indizes

---
+++ 0101_CachedPlans.sql
@@ -0,0 +1,15 @@
+-- Retrieve the different cached Execution Plans
+
+dbcc freeproccache
+ALTER DATABASE SCOPED CONFIGURATION CLEAR PROCEDURE_CACHE;
+
+
+
+SELECT
+	st.text, 
+	qp.query_plan, 
+	qs.*
+From sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) st
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+WHERE  text like '%parttab%'
